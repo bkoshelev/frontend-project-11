0349335a89839b10e9836349579831d99de1535d
bkoshelev <brskoshelev@gmail.com>
2022-08-19 14:29:02 +0300

    etract styles

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-### Hexlet tests and linter status:
+# RSS Reader
 
 [![Actions Status](https://github.com/bkoshelev/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/bkoshelev/frontend-project-11/actions)
-[![Node CI](https://github.com/bkoshelev/frontend-project-11/workflows/nodejs.yml/badge.svg)](https://github.com/hexlet-boilerplates/nodejs-package/actions)
+[![Node CI](https://github.com/bkoshelev/frontend-project-11/actions/workflows/nodejs.yml/badge.svg)](https://github.com/bkoshelev/frontend-project-11/actions/workflows/nodejs.yml)
 [![Maintainability](https://api.codeclimate.com/v1/badges/d74d66478befe1d63640/maintainability)](https://codeclimate.com/github/bkoshelev/frontend-project-11/maintainability)
 
 [Ссылка на готовый проект](https://frontend-project-11.vercel.app/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,32 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     mode: process.env.NODE_ENV || 'development',
     module: {
         rules: [
             {
-                test: /\.(scss)$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: () => [
-                                    require('autoprefixer')
-                                ]
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            }
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
+        new MiniCssExtractPlugin(),
     ],
     devServer: {
         static: path.resolve(__dirname, 'dist'),
         port: 8080,
-        hot: true
-    }
+        hot: true,
+    },
 };
